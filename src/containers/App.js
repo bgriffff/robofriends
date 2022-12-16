89e5f51e81831ccc206497fac68cc3477a49a353
bgriffff <brandonjosephgriffiths@gmail.com>
2022-12-16 11:37:34 -0700

    added hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,52 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super()
-        // this state now owns robots so it can change it
-        this.state = {
-            robots: [],
-            searchfield : ''
-        }
-    }
+function App() {
+    //setRobots is the function that will change robots
+    // put in the useState() the initial value you want
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+    const [count, setCount] = useState(0)
 
-    componentDidMount() {
+
+    //everytime this app is run it calls the useEffect. if you dont add the 
+    // [] at the end of useEffect or it will be an infinite loop 
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(user => {this.setState({ robots: user})});
+            .then(user => {setRobots(user)});    
+            console.log(count)   
+        }, [count]) // only run if count changes
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value})
-    }
-
-    render(){
-        const {robots, searchfield} = this.state
-        //accessing the data and filtering the array
-        const filteredRobots = robots.filter(robot => {
-            //returns lowercase robots name if it includes the searchfield in lowercase
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        if (robots.length === 0) {
-            return <h1>Loading...</h1>
-        } else {
-            return(
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-        }
+    //accessing the data and filtering the array
+    const filteredRobots = robots.filter(robot => {
+        //returns lowercase robots name if it includes the searchfield in lowercase
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    if (robots.length === 0) {
+        return <h1>Loading...</h1>
+    } else {
+        return(
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <button onClick={() =>setCount(count+1)}>Click Me!</button>
+                <p className='f1'>{count}</p>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
     }
 }
 
